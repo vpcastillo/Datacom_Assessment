e9c13b0a99adfdf55fdf3c3e737779542fd86882
vpcastillo <vio_castillo@yahoo.com>
2022-04-07 20:11:09 +0800

    v2.0.3

--- Datacom/API_Test_Automation.js
+++ Datacom/API_Test_Automation.js
@@ -4,7 +4,10 @@ const { url } = require("inspector")
 
 describe('Get API User Test', () => 
 {
-    let homeurl = 'http://jsonplaceholder.typicode.com/users/'
+    const homeURL = 'http://jsonplaceholder.typicode.com/users/'
+    const userid = '/8'
+    const uName = 'Nicholas Runolfsdottir V'
+
     
     it('TC1', () => 
     {
@@ -12,7 +15,7 @@ describe('Get API User Test', () =>
         cy.request({
 
             method : 'GET',
-            url : homeurl
+            url : homeURL
 
         }).then((response) =>{
             expect(response.status).to.eq(200)
@@ -26,53 +29,66 @@ describe('Get API User Test', () =>
         cy.request({
 
             method : 'GET',
-            url : homeurl+'/8'
+            url : homeURL+userid
 
         }).then((response) =>{
             expect(response.status).to.eq(200)
-            expect(response.body).has.property('name', 'Nicholas Runolfsdottir V')
+            expect(response.body).has.property('name', uName)
         })
 
     })
 
     it('TC3', () => 
     {
-        cy.log('Verify POST Users request')        
         cy.request({
 
-            method : 'POST',
-            url : homeurl,
-
-            body : {
-                    "id": "11",
-                    "name": "Jotaro Kujo",
-                    "username": "Jojo",
-                    "email": "star@platinum.stand",
-                    "address": {
-                        "street": "Kulas Light",
-                        "suite": "Apt. 556",
-                        "city": "Gwenborough",
-                        "zipcode": "92998-3874",
-                        "geo": {
-                            "lat": "-37.3159",
-                            "lng": "81.1496"
-                        }
-                    },
-                    "phone": "1-770-736-8031 x56442",
-                    "website": "hildegard.org",
-                    "company": {
-                        "name": "Romaguera-Crona",
-                        "catchPhrase": "Multi-layered client-server neural-net",
-                        "bs": "harness real-time e-markets"
-                    }
-                }
+            method : 'GET',
+            url : homeURL
 
         }).then((response) =>{
-            expect(response.status).to.eq(201)
-            expect(response.body).has.property('id', 11)
-            expect(response.body).has.property('name', 'Jotaro Kujo')
-            expect(response.body).has.property('username', 'Jojo')
-            expect(response.body).has.property('email', 'star@platinum.stand')
+            expect(response.status).to.eq(200)
+            expect(response.body.length).to.equal(10)
+
+            let uId = response.body.length +1            
+
+            cy.log('Verify POST Users request')        
+            cy.request({
+
+                method : 'POST',
+                url : homeURL,
+
+                body : {
+                        "id": uId,
+                        "name": "Jotaro Kujo",
+                        "username": "Jojo",
+                        "email": "star@platinum.stand",
+                        "address": {
+                            "street": "Kulas Light",
+                            "suite": "Apt. 556",
+                            "city": "Gwenborough",
+                            "zipcode": "92998-3874",
+                            "geo": {
+                                "lat": "-37.3159",
+                                "lng": "81.1496"
+                            }
+                        },
+                        "phone": "1-770-736-8031 x56442",
+                        "website": "hildegard.org",
+                        "company": {
+                            "name": "Romaguera-Crona",
+                            "catchPhrase": "Multi-layered client-server neural-net",
+                            "bs": "harness real-time e-markets"
+                        }
+                    }
+
+            }).then((response) =>{
+                cy.log(JSON.stringify(response))
+                expect(response.status).to.eq(201)
+                expect(response.body).has.property('id', 11)
+                expect(response.body).has.property('name', 'Jotaro Kujo')
+                expect(response.body).has.property('username', 'Jojo')
+                expect(response.body).has.property('email', 'star@platinum.stand')
+            })
         })
     })
 })
